81e67fcbfc0f2ada3c57e84dd93b15cdeaa9b1ea
Justin Chambers <justin@broadviewtechnicalsolutions.com>
2022-07-06 08:46:41 -0400

    Add depolyment settings

---
+++ PRP/PRP.Client/server.js
@@ -0,0 +1,20 @@
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl).then();
+  }).listen(3000, (err) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log("Server started");
+  });
+});
