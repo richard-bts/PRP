c598783cb1568a4c09386d0275732f81bddbb9f2
Oyedele-Sg <oyedelesegunfunmi@gmail.com>
2022-07-06 14:45:03 -0500

    Removed unused PRP_Client dir

--- PRP_Client/src/shared/components/PageTitle.js
+++
@@ -1,5 +0,0 @@
-export const PageTitle = ({ title }) => {
-  return (
-    <h2 className="text-2xl font-bold uppercase">{ title }</h2>
-  )
-}
--- PRP_Client/src/store/index.js
+++
@@ -1,3 +0,0 @@
-export * from './store';
-export * from './sidebar/sidebarSlice';
-export * from './partners/partnertsSlice';
