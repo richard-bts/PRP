7039cde718282a3d42e901712081cbf903b1c390
Oyedele-Sg <oyedelesegunfunmi@gmail.com>
2022-08-22 16:21:42 -0500

    add client-dev code

--- PRP/PRP.Service.Api/Models/Dto/UpdatePartnerReportTypeDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PRP.Service.Api.Models.Dto
-{
-    public class UpdatePartnerReportTypeDto
-    {
-        public int partner_id { get; set; }
-        public int report_type_id { get; set; }
-        public int active { get; set; }
-    }
-}
--- PRP/PRP.Service.Api/Models/PartnerReportType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PRP.Service.Api.Models
-{
-    public class PartnerReportType
-    {
-        public int report_type_id { get; set; }
-        public string? report_name { get; set; }
-        public int active { get; set; }
-    }
-}
