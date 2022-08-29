67b10127f63797e8d7c7dff754ae2067c818e470
Justin Chambers <justin@broadviewtechnicalsolutions.com>
2022-08-29 11:30:25 -0400

    Update dropdown

--- client/src/store/partners/thunks.js
+++ client/src/store/partners/thunks.js
@@ -87,7 +87,7 @@ export const getReportTypes = async() => {
 
 export const getCompanysName = createAsyncThunk("partners/getCompanysName", async(name, { dispatch }) => {
   dispatch(setLoading(true));
-  const url = `https://http://172.24.32.132/Xcelerator/prpserver/api/report/GetCompanyName?Name=${name}`
+  const url = `http://172.24.32.132/Xcelerator/prpserver/api/report/GetCompanyName?Name=${name}`
   const response = await fetch(url);
   const { result } = await response.json();
   dispatch(setLoading(false));
