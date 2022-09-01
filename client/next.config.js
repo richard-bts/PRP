1bc3993c83839a9c15dc6e07eb05968db9501330
Broadview Tech <btech209@ad.cdldelivers.com>
2022-09-01 15:47:57 -0400

    creating autod-deployment

--- client/next.config.js
+++ client/next.config.js
@@ -1,21 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
-const prodPath = '/Xcelerator/CDLPRP'
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  assetPrefix: isProd ? prodPath : '/',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    loader: 'akamai',
-    path: isProd ? prodPath : '/',
-  },
-  env: {
-    linksPath: isProd ? prodPath : '',
-  }
+  swcMinify: true
 }
 
 module.exports = nextConfig
