5a8cba0d7dc3d5717e6a97d3e2a5f440a276cc4a
Fred Santos <fred@broadviewtechnicalsolutions.com>
2022-09-09 10:29:08 -0400

    Enabled CORS on IndentityServer

--- PRP.IndetityServer/Program.cs
+++ PRP.IndetityServer/Program.cs
@@ -2,12 +2,6 @@ using Microsoft.Extensions.DependencyInjection;
 using PRP.IndetityServer.config;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddIdentityServer()
-                .AddInMemoryClients(InMemoryConfig.GetClients())
-                .AddInMemoryIdentityResources(InMemoryConfig.GetIdentityResources())
-                .AddInMemoryApiResources(InMemoryConfig.GetResources)
-                .AddInMemoryApiScopes(InMemoryConfig.GetScopes)
-                .AddDeveloperSigningCredential();
 
 builder.Services.AddCors(options =>
 {
@@ -18,6 +12,14 @@ builder.Services.AddCors(options =>
         });
 });
 
+builder.Services.AddIdentityServer()
+                .AddInMemoryClients(InMemoryConfig.GetClients())
+                .AddInMemoryIdentityResources(InMemoryConfig.GetIdentityResources())
+                .AddInMemoryApiResources(InMemoryConfig.GetResources)
+                .AddInMemoryApiScopes(InMemoryConfig.GetScopes)
+                .AddDeveloperSigningCredential();
+
 var app = builder.Build();
 app.UseIdentityServer();
+app.UseCors();
 app.Run();
